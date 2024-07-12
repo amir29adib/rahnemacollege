8d09d9e3662408769df9fa6dd3d00226d83a05ba
Amir <amir29adib1234@gmail.com>
2024-07-12 22:45:09 +0330

    fix bug of user login route

--- Referendum/src/app.ts
+++ Referendum/src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import { getUser } from './user';
 
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
@@ -12,9 +15,9 @@ app.get('/', (req, res) => {
     res.send(JSON.stringify({message: 'Welcome! Please go to route user/login for login!'}));
 });
 
-app.get('/user/login', (req, res) => {
-    const username = req.query.username?.toString();
-    const password = req.query.password?.toString();
+app.post('/user/login', (req, res) => {
+    const username = req.body.username?.toString();
+    const password = req.body.password?.toString();
     
     if (!username || !password) {
         return res.status(400).send(JSON.stringify({ message: 'Username and password are required' }));
